c7c0965e8d897fa337176605d8f835f2626d3f5f
rmk <rachaelmkunz@gmail.com>
2021-08-17 12:40:26 +0200

    add prevent-scroll toggle when menu is open

--- script/app.js
+++ script/app.js
@@ -3,7 +3,8 @@ const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => {
-  //   hamburger.classList.toggle("open");
   menu.classList.toggle("active");
   overlay.classList.toggle("overlay");
+  //prevent body scroll Y when menu is open
+  document.body.classList.toggle("prevent-scroll");
 });
